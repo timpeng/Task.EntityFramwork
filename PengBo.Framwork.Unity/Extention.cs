4637a7f666d8ea17ba6e1173b037beaca4bc5222
timpeng <1500824597@qq.com>
2015-12-03 18:45:16 +0800

    代码更新
    准备加入WCF，相关调用

--- PengBo.Framwork.Unity/Extention.cs
+++ PengBo.Framwork.Unity/Extention.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace PengBo.Framwork.Unity
 {
@@ -15,10 +16,32 @@ namespace PengBo.Framwork.Unity
                 action(key);
             }
         }
-
         public static string Append(this string value, string append)
         {
             return value + append;
+        }
+        public static string TryToString(this object value)
+        {
+            return Convert.ToString(value);
+        }
+        public static int TryToInt(this object value)
+        {
+            int num;
+            if (int.TryParse(value.TryToString(), out num))
+            {
+                return num;
+            }
+            throw new Exception("当前值不能转为int32");
+        }
+
+        public static T DeserializeObjectToJson<T>(this string value)
+        {
+            return JsonConvert.DeserializeObject<T>(value);
+        }
+
+        public static string DeserializeObjectToJson<T>(this T t) where T : class,new()
+        {
+            return JsonConvert.SerializeObject(t);
         }
     }
 }
